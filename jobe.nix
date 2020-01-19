136ace688ddd4cccb230397a8872856f1087a6f0
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2020-01-19 13:37:53 -0800

    Open up the port minecraft server uses

--- jobe.nix
+++ jobe.nix
@@ -44,8 +44,8 @@ in
   services.samba.syncPasswordsByPam = true;
 
   # Open up samba ports
-  networking.firewall.allowedTCPPorts = [ 137 138 139 445 ];
-  networking.firewall.allowedUDPPorts = [ 137 138 139 445 ];
+  networking.firewall.allowedTCPPorts = [ 137 138 139 445 25565 ];
+  networking.firewall.allowedUDPPorts = [ 137 138 139 445 25565 ];
 
   # Drivers
   services.xserver.videoDrivers = ["nvidia"];
