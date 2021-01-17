9ffdfbcd92fde82aa1e4971235352498cce1ec5a
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2021-01-16 22:53:03 -0800

    Remove stumpwm -- I wasn't using it anyway

--- ui.nix
+++ ui.nix
@@ -1,12 +1,8 @@
 { config, pkgs, ... }:
 {
   services.xserver.enable = true;
-  # services.xserver.desktopManager.gnome3.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.displayManager.gdm.wayland = false;
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.windowManager.stumpwm.enable = true;
 
   services.xserver.desktopManager.gnome3.sessionPath = [
     pkgs.gnomeExtensions.appindicator
