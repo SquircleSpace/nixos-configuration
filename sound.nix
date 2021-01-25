07ea6c1e368e9cb84e62af495c10fe949de36504
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2021-01-24 23:18:25 -0800

    sound: enable dconf (pulse uses it to preserve some prefs)

--- sound.nix
+++ sound.nix
@@ -1,7 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
   hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ];
+  programs.dconf.enable = lib.mkDefault true;
 }
