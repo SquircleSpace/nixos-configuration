f79d52d285e3613364d01b7be506ebd30866f0e7
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2021-03-16 21:44:50 -0700

    Libbie install 32bit drivers for steam

--- libbie.nix
+++ libbie.nix
@@ -116,6 +116,9 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  hardware.opengl.driSupport32Bit = true;
+  hardware.pulseaudio.support32Bit = true;
+
   users.extraUsers.ada.packages = with pkgs; [
     discord
     steam
