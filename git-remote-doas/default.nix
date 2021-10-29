4db8292c8ac85a4dff2b25618cecc626c8d21725
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2021-10-29 14:30:50 -0700

    git-remote-doas: List GHC as a build input

--- git-remote-doas/default.nix
+++ git-remote-doas/default.nix
@@ -3,6 +3,7 @@ stdenv.mkDerivation rec {
   name = "git-remote-doas-${version}";
   version = "1.0.0";
   src = ./.;
+  buildInputs = [ ghc ];
   buildPhase = ''
     ${ghc}/bin/ghc -o git-remote-doas Main.hs
   '';
