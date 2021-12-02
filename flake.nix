17fbb5b6d0069506c956df2f8f0d1b6478a3d710
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2021-12-02 00:49:35 -0800

    Flake: Add NixOS 21.11 flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   inputs.dwarffs.url = "github:edolstra/dwarffs";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.nixpkgs2105.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.nixpkgs2111.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.nix.url = "github:NixOS/nix";
   inputs.rss4email.url = "github:SquircleSpace/rss4email";
-  inputs.rss4email.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.rss4email.inputs.nixpkgs.follows = "nixpkgs2105";
 
-  outputs = { self, nixpkgs, dwarffs, nix, rss4email }:
+  outputs = { self, nixpkgs2105, nixpkgs2111, dwarffs, nix, rss4email }:
     let
-      revisionModule = {...}: {
+      revisionModule = nixpkgs: {...}: {
         system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
       };
-      nixpkgsPinModule = {...}: {
+      nixpkgsPinModule = nixpkgs: {...}: {
         nix.registry.nixpkgs.flake = nixpkgs;
       };
-      nixosSystem = cfg: nixpkgs.lib.nixosSystem (cfg // {
-        modules = cfg.modules ++ [ revisionModule nixpkgsPinModule rss4email.nixosModule ];
+      nixosSystem = nixpkgs: cfg: nixpkgs.lib.nixosSystem (cfg // {
+        modules = cfg.modules ++ [
+          (revisionModule nixpkgs)
+          (nixpkgsPinModule nixpkgs)
+          rss4email.nixosModule
+        ];
       });
     in {
-      nixosConfigurations.Jobe = nixosSystem {
+      nixosConfigurations.Jobe = nixosSystem nixpkgs2105 {
         system = "x86_64-linux";
         modules = [
           ./jobe.nix
@@ -26,7 +31,7 @@
         ];
       };
 
-      nixosConfigurations.Libbie = nixosSystem {
+      nixosConfigurations.Libbie = nixosSystem nixpkgs2105 {
         system = "x86_64-linux";
         modules = [
           ./libbie.nix
@@ -35,7 +40,7 @@
         ];
       };
 
-      nixosConfigurations.pifer = nixosSystem {
+      nixosConfigurations.pifer = nixosSystem nixpkgs2105 {
         system = "aarch64-linux";
         modules = [ ./pifer.nix ];
       };
