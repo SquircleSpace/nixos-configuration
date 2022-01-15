7b7696405c7b106f08f45961c1692bf2e6b56fde
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2022-01-14 20:06:30 -0800

    pi-hole: Remove web password
    The service is set up.  I don't need access to the admin panel any
    more, and having a stupid password like this in the config is at best
    attack surface.

--- pi-hole.nix
+++ pi-hole.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ... }:
 {
   virtualisation.oci-containers.containers."pi-hole" = {
-    environment = {
-      WEBPASSWORD = "testing";
-    };
     image = "pihole/pihole:latest";
     ports = [
       "53:53/tcp"
