156da0294ea3875a284983f78b2df2129a9761f0
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2022-01-15 13:52:40 -0800

    Pi-Hole: Tell the container what its host's IP is
    This should make pi.hole resolve correctly

--- pi-hole.nix
+++ pi-hole.nix
@@ -2,6 +2,9 @@
 {
   virtualisation.oci-containers.containers."pi-hole" = {
     image = "pihole/pihole:latest";
+    environment = {
+      FTLCONF_REPLY_ADDR4 = "192.168.1.142";
+    };
     ports = [
       "53:53/tcp"
       "53:53/udp"
