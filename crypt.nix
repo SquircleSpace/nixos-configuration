1b3c25a3009a7fe3d25aaeccf5be846d3a6fa7b6
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2022-12-02 20:07:12 -0800

    Crypt: init

---
+++ crypt.nix
@@ -0,0 +1,84 @@
+{ config, lib, pkgs, ... }:
+{
+  imports = [
+    ./crypt-hardware.nix
+    ./common.nix
+    ./ada.nix
+    ./tailscale.nix
+  ];
+
+  boot.loader.grub.enable = false;
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 4;
+  boot.loader.raspberryPi.uboot.configurationLimit = 3;
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/205878f5-95f0-4cbe-940a-e8da7f9d93da";
+    fsType = "btrfs";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/D674-B75E";
+    fsType = "vfat";
+  };
+
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 5d";
+  nix.gc.dates = "Mon *-*-* 00:03:15";
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.flake = "/etc/nixos";
+  system.autoUpgrade.allowReboot = false;
+  system.autoUpgrade.dates = "Mon *-*-* 00:04:40";
+  services.flakeAutoUpdate."/etc/nixos".enable = true;
+  environment.etc."nixos/.git/hooks/post-receive" = {
+    enable = true;
+    source = pkgs.etc-nixos-post-receive-hook;
+  };
+
+  boot.consoleLogLevel = lib.mkDefault 7;
+
+  networking.hostName = "crypt";
+
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.eth0.tempAddress = "disabled";
+  networking.interfaces.wlan0.useDHCP = true;
+  networking.interfaces.wlan0.tempAddress = "disabled";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  time.timeZone = "America/Los_Angeles";
+
+  environment.systemPackages = with pkgs; [
+    emacs-nox tmux rsync
+  ];
+
+  services.openssh.enable = true;
+  services.openssh.passwordAuthentication = false;
+  services.openssh.permitRootLogin = "no";
+
+  services.fail2ban.enable = true;
+  services.fail2ban.bantime-increment.enable = true;
+  services.fail2ban.bantime-increment.maxtime = "30d";
+  services.fail2ban.jails = {
+    sshd = ''
+      mode = aggressive
+      enabled = true
+      port = ${lib.concatMapStringsSep "," (p: toString p) config.services.openssh.ports}
+    '';
+  };
+
+  users.mutableUsers = false;
+
+  users.users.ada = {
+    uid = 1000;
+    openssh.authorizedKeys.keyFiles = [ ./jobe.pub ./phone.pub ./libbie.pub ];
+  };
+
+  system.stateVersion = "22.05";
+
+  networking.networkmanager.enable = true;
+
+  networking.firewall.enable = true;
+}
