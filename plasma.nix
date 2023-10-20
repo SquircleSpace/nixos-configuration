362693e06083f3f952513529b2cdb0ebd9fe6839
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2023-10-20 23:27:06 +0000

    Plasma: init

---
+++ plasma.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./libbie-hardware.nix
+    ./common.nix
+    ./ui.nix
+    ./ada.nix
+    ./sasha.nix
+    ./ssh.nix
+    ./tailscale.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [ "/" ];
+  };
+
+  networking.hostName = "Plasma";
+
+  time.timeZone = "America/Los_Angeles";
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  services.xserver.libinput.enable = true;
+
+  users.mutableUsers = false;
+
+  powerManagement.powertop.enable = true;
+  networking.networkmanager.wifi.powersave = true;
+
+  services.logind.lidSwitch = "suspend";
+  services.logind.lidSwitchDocked = "ignore";
+  services.logind.lidSwitchExternalPower = "suspend";
+
+  system.stateVersion = "23.05";
+
+  services.flakeAutoUpdate."/etc/nixos".enable = true;
+  environment.etc."nixos/.git/hooks/post-receive" = {
+    enable = true;
+    source = pkgs.etc-nixos-post-receive-hook;
+  };
+
+  documentation.dev.enable = true;
+  documentation.man.generateCaches = true;
+
+  services.flatpak.enable = true;
+}
