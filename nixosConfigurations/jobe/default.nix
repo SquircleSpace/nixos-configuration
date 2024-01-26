670359a7111694c1a1b2479e4bddfef7439cf522
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2024-01-26 00:45:34 -0800

    Jobe: Update to 23.11

--- nixosConfigurations/jobe/default.nix
+++ nixosConfigurations/jobe/default.nix
@@ -1,6 +1,6 @@
-{ self, mkNixosSystem, nixpkgs2305 }:
+{ self, mkNixosSystem, nixpkgs2311 }:
 mkNixosSystem {
-  nixpkgs = nixpkgs2305;
+  nixpkgs = nixpkgs2311;
   system = "x86_64-linux";
   modules = [
     self.nixosModules.default
